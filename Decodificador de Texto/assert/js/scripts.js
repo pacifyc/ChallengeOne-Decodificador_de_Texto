e9585225306014a451a004b24b965fa1c0d4bacc
Silvio_Sena <silviosenna.jl@gmail>
2023-01-04 19:57:23 -0300

    Criação de Modal
    Foi efetuada a criação de um modal com objetivo de exibir uma mensagem

--- Decodificador de Texto/assert/js/scripts.js
+++ Decodificador de Texto/assert/js/scripts.js
@@ -1,28 +1,69 @@
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+
+closeModalButton.addEventListener("click", function () {
+
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+
+})
+
+
 const btn = document.querySelector("#codificar");
+document.querySelector("#recebe").value = " ";
+document.getElementById("recebe").focus();
+//var name = document.querySelector("#recebe");
 
-btn.addEventListener("click", function(){
+const valor = document.querySelector("#recebe").value;
+console.log(valor.length);
 
-    const name = document.querySelector("#recebe");
+btn.addEventListener("click", function () {
 
-    const valor = name.value;
+  //document.getElementById("recebe").focus();
 
-    console.log(valor);
+  const valor = document.querySelector("#recebe").value;
+
+  //const valor = name.value;
+
+  if (valor.length < 3) {
+
+    console.log(typeof(valor));
+    console.log(valor.length);
+
+    console.log("valor vazio");
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+
+
+  } else {
+
+
+    console.log(valor.length);
+
+    console.log("esse é o valor", valor);
 
     document.querySelector("#exibe").value = valor;
-    
-    document.querySelector("#recebe").value = " ";
+
+
+  }
+
+
+
+  document.querySelector("#recebe").value = " ";
 
 });
 
 
 let btnCopia = document.querySelector('#copiar');
 
-btnCopia.addEventListener('click', function() {
+btnCopia.addEventListener('click', function () {
 
   let textArea = document.querySelector('#exibe');
 
   textArea.select();
 
   document.execCommand('copy');
-  
+
 });
